f47213b1f58b1aaaca179551605f507ae19736df
kondrusina <kondrusina@rambler.ru>
2022-11-22 16:34:39 +0300

    fix and add code  js file

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+document.getElementById('main-action').onclick = function () {
+    document.getElementById('cars').scrollIntoView({ behavior: 'smooth' });
+}
+
+let buttons = document.getElementsByClassName('car-button');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById('price').scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+document.getElementById('price-action').onclick = function () {
+    if (document.getElementById('name').value === '') {
+        alert('Заполните поле Имя!');
+    } else if (document.getElementById('phone').value === '') {
+        alert('Заполните поле Телефон!');
+    } else if (document.getElementById('car').value === '') {
+        alert('Заполните поле Автомобиль!');
+    } else
+        alert('Спасибо за заявку, мы свяжемся с вами в ближайшее время!')
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     let layer = document.querySelector('.price-image');
     document.addEventListener('mousemove', (event) => {
-        layer.style.transform = 'translate3d(' + ((event.clientX * 0.5) / 10) + 'px,' + ((event.clientY * 0.5) / 10) + 'px,0px)';
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.3) / 6) + 'px,' + ((event.clientY * 0.3) / 8) + 'px,0px)';
     });
 
     const elem = document.querySelector(".main");
@@ -9,3 +31,13 @@ document.addEventListener("DOMContentLoaded", function () {
         elem.style.backgroundPositionX = '0' + (0.3 * window.pageYOffset) + 'px';
     })
 });
+
+
+
+
+
+
+
+
+
+
